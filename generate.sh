05b06fae6eada41d3c5ba219acd80420c886e016
建力 <1879324764@qq.com>
2025-09-07 10:10:10 +0800

    add conver script

---
+++ generate.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# @Project Name : oslo.$1
+# @Author       : hjl
+# @Time         : 2025/9/6 21:37
+# @File         : generate.sh
+# @Software     : PyCharm
+# @Description  : scripts function describe
+
+if [ $# -ne 2 ]; then
+  echo "⚠️ Error: Invalid arguments."
+  echo "Usage: $0 <old_project_name> <project_name>"
+  exit 1
+fi
+
+PROJECT_ROOT="."
+old_project_name=$1
+project_name=$2
+
+find . -type f -name "*.pyc" -exec rm -f {} \;
+find . -type d -name "__pycache__" -exec rm -rf {} \;
+
+function convert_project_dir_name() {
+    local old_project_name=$1
+    local project_name=$2
+    find "${PROJECT_ROOT}" -depth -type d -a -not -path "./.git/*" \
+    -a -not -path "./.idea/*" \
+    -a -not -path "./venv/*" \
+    -a -not -path "./.vscode/*" \
+    -name "*${old_project_name}*" -print0  | while IFS= read -r -d '' dir; do
+        dir_name=$(basename "${dir}")
+        new_dir_name="${dir_name//${old_project_name}/${project_name}}"
+
+        if [ "${dir_name}" = "${new_dir_name}" ]; then
+            continue
+        fi
+
+        new_dir_path="${dir%/${dir_name}}/${new_dir_name}"
+        if [ -e "${new_dir_path}" ]; then
+            continue
+        fi
+
+        echo " 🔄 重命名目录：${dir} → ${new_dir_path}"
+        if ! mv -v "${dir}" "${new_dir_path}"; then
+            echo "❌ 重命名失败：${dir} → ${new_dir_path}"
+        fi
+    done
+
+}
+
+function convert_project_file_name() {
+    local old_project_name=$1
+    local project_name=$2
+    find "${PROJECT_ROOT}" -depth -type f  \
+    -a -not -path "./.git/*" \
+    -a -not -path "./.idea/*" \
+    -a -not -path "./venv/*" \
+    -a -not -path "./.vscode/*" \
+    -type f -name "*${old_project_name}*" | while read -r file; do
+    new_file="${file//${old_project_name}/${project_name}}"
+    echo " 🔄 重命名文件：${file} → ${new_file}"
+    mv -v "$file" "$new_file"
+done
+}
+
+# 替换项目目录下所有文件内容
+function convert_project_file_content() {
+    local old_project_name=$1
+    local project_name=$2
+#    grep -Erl "*${old_project_name}*" --exclude-dir={.git,.idea} .
+    if grep -Erq "*${old_project_name}*" --exclude-dir={.git,.idea} .; then
+        echo " 🔄 替换文件内容：${old_project_name} → ${project_name}"
+        grep -Erl "*${old_project_name}*" --exclude-dir={.git,.idea}  . | xargs -n 1 -t sed -i "s/${old_project_name}/${project_name}/g"
+    else
+        echo "⚠️ Error: No files containing the old project name found."
+    fi
+}
+
+function main() {
+    convert_project_dir_name $old_project_name $project_name
+    sleep 1
+    convert_project_file_name $old_project_name $project_name
+    sleep 1
+    convert_project_file_content $old_project_name $project_name
+}
+
+main $*
--- prototype/prototype/version.py
+++ prototype/prototype/version.py
@@ -1,4 +1,4 @@
-#    Copyright 2011 OpenStack Foundation
+#    Copyright 2012 OpenStack Foundation
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -12,80 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+
 import pbr.version
 
-from prototype.common.i18n import _LE
-
-PROTOTYPE_VENDOR = "OpenStack Foundation"
-PROTOTYPE_PRODUCT = "OpenStack Prototype"
-PROTOTYPE_PACKAGE = None  # OS distro package version suffix
-
-loaded = False
 version_info = pbr.version.VersionInfo('prototype')
-version_string = version_info.version_string
-
-
-def _load_config():
-    # Don't load in global context, since we can't assume
-    # these modules are accessible when distutils uses
-    # this module
-    from six.moves import configparser
-
-    from oslo_config import cfg
-
-    from oslo_log import log as logging
-
-    global loaded, PROTOTYPE_VENDOR, PROTOTYPE_PRODUCT, PROTOTYPE_PACKAGE
-    if loaded:
-        return
-
-    loaded = True
-
-    cfgfile = cfg.CONF.find_file("release")
-    if cfgfile is None:
-        return
-
-    try:
-        cfg = configparser.RawConfigParser()
-        cfg.read(cfgfile)
-
-        PROTOTYPE_VENDOR = cfg.get("Prototype", "vendor")
-        if cfg.has_option("Prototype", "vendor"):
-            PROTOTYPE_VENDOR = cfg.get("Prototype", "vendor")
-
-        PROTOTYPE_PRODUCT = cfg.get("Prototype", "product")
-        if cfg.has_option("Prototype", "product"):
-            PROTOTYPE_PRODUCT = cfg.get("Prototype", "product")
-
-        PROTOTYPE_PACKAGE = cfg.get("Prototype", "package")
-        if cfg.has_option("Prototype", "package"):
-            PROTOTYPE_PACKAGE = cfg.get("Prototype", "package")
-    except Exception as ex:
-        LOG = logging.getLogger(__name__)
-        LOG.error(_LE("Failed to load %(cfgfile)s: %(ex)s"),
-                  {'cfgfile': cfgfile, 'ex': ex})
-
-
-def vendor_string():
-    _load_config()
-
-    return PROTOTYPE_VENDOR
-
-
-def product_string():
-    _load_config()
-
-    return PROTOTYPE_PRODUCT
-
-
-def package_string():
-    _load_config()
-
-    return PROTOTYPE_PACKAGE
-
-
-def version_string_with_package():
-    if package_string() is None:
-        return version_info.version_string()
-    else:
-        return "%s-%s" % (version_info.version_string(), package_string())
